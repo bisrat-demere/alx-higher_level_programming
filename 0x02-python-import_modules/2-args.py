1032977b238f22968f96b9bba5f84c7ad95a277f
bisrat-demere <bisratdemere@gmail.com>
2022-06-02 21:54:16 +0300

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
         print("1 argument:")
         print("1: {}".format(sys.argv[1]))
     else:
-        print("{}: arguments:".format(n-1))
+        print("{} arguments:".format(n-1))
         for y in range(1, len(sys.argv)):
             print("{}: {}".format(y, sys.argv[y]))
