7f0201e341ce5cd9410c7d24c202444eacfbd94a
bisrat-demere <bisratdemere@gmail.com>
2022-06-21 22:11:15 +0300

    100and103

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,23 +1,12 @@
 #!/usr/bin/python3
-"""Define classes for a singly-linked list."""
-
-
 class Node:
-    """Represent a node in a singly-linked list."""
-
     def __init__(self, data, next_node=None):
-        """Initialize a new Node.
-        Args:
-            data (int): The data of the new Node.
-            next_node (Node): The next node of the new Node.
-            """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """Get/set the data of the Node."""
-        return (self.__data)
+        return self.__data
 
     @data.setter
     def data(self, value):
@@ -25,48 +14,44 @@ class Node:
             raise TypeError("data must be an integer")
         self.__data = value
 
-        @property
-        def next_node(self):
-            """Get/set the next_node of the Node."""
-            return (self.__next_node)
+    @property
+    def next_node(self):
+        return self.__next_node
 
-        @next_node.setter
-        def next_node(self, value):
-            if not isinstance(value, Node) and value is not None:
-                raise TypeError("next_node must be a Node object")
-            self.__next_node = value
+    @next_node.setter
+    def next_node(self, value):
+        if not isinstance(value, Node) and value is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
 
 
 class SinglyLinkedList:
-    """ singly linked list class"""
+    def __str__(self):
+        rtn = ""
+        ptr = self.__head
+
+        while ptr is not None:
+            rtn += str(ptr.data)
+            if ptr.next_node is not None:
+                rtn += "\n"
+            ptr = ptr.next_node
+
+        return rtn
 
     def __init__(self):
-        """ initialization"""
         self.__head = None
 
-    def __str__(self):
-        """ make list printable"""
-        all_data = []
-        tmp = self.__head
-        while (tmp):
-            all_data.append(str(tmp.data))
-            tmp = tmp.next_node
-        # return all data except the new line at the end
-        return ('\n'.join(all_data))
-
     def sorted_insert(self, value):
-        """ insert into correct sorted position"""
-        NewNode = Node(value)
-        temp = self.__head
-        if temp is None:
-            self.__head = NewNode
-            return
-        if value <= temp.data:
-            NewNode.next_node = self.__head
-            self.__head = NewNode
-            return
+        ptr = self.__head
 
-        while (temp.next_node is not None and temp.next_node.data <= value):
-            temp = temp.next_node
-        NewNode.next_node = temp.next_node
-        temp.next_node = NewNode
+        while ptr is not None:
+            if ptr.data > value:
+                break
+            ptr_prev = ptr
+            ptr = ptr.next_node
+
+        newNode = Node(value, ptr)
+        if ptr == self.__head:
+            self.__head = newNode
+        else:
+            ptr_prev.next_node = newNode
